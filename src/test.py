2a61b0b540a71248ee54328b8397fbb74169a130
Zhang Jiaqi <marycheung021213@gmail.com>
2025-03-25 14:39:59 +0800

    Seperate utils

--- src/test.py
+++ src/test.py
@@ -5,8 +5,10 @@ import torch
 from tqdm.auto import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+from utils.transformer_util import extract_embeddings, fetch_similar
 
-# Load the model and the feature extractor
+# Load image encoder
 model_ckpt = "nateraw/vit-base-beans"
 extractor = AutoFeatureExtractor.from_pretrained(model_ckpt)
 model = AutoModel.from_pretrained(model_ckpt)
@@ -23,10 +25,10 @@ for i, label in enumerate(labels):
     id2label[i] = label
 
 num_samples = 100
-seed = 42
+seed = random.randint(0, 1000)
 candidate_subset = dataset["train"].shuffle(seed=seed).select(range(num_samples))
 
-# Data transformation chain.
+# Extract embeddings for the candidate images
 transformation_chain = T.Compose(
     [
         # We first resize the input image to 256x256 and then we take center crop.
@@ -37,33 +39,10 @@ transformation_chain = T.Compose(
     ]
 )
 
-
-def extract_embeddings (model: torch.nn.Module):
-    """Utility to compute embeddings."""
-    device = model.device
-
-    def pp (batch):
-        images = batch ["image"]
-        # `transformation_chain` is a compostion of preprocessing
-        # transformations we apply to the input images to prepare them
-        # for the model. For more details, check out the accompanying Colab Notebook.
-        image_batch_transformed = torch.stack (
-            [transformation_chain (image) for image in images]
-        )
-        new_batch = {"pixel_values": image_batch_transformed.to (device)}
-        with torch.no_grad ():
-            embeddings = model (**new_batch).last_hidden_state [:, 0].cpu ()
-        return {"embeddings": embeddings}
-
-    return pp
-
-
-# Here, we map embedding extraction utility on our subset of candidate images.
 batch_size = 24
 device = "cuda" if torch.cuda.is_available() else "cpu"
-extract_fn = extract_embeddings(model.to(device))
+extract_fn = extract_embeddings(model.to(device), transformation_chain)
 candidate_subset_emb = candidate_subset.map(extract_fn, batched=True, batch_size=24)
-
 
 candidate_ids = []
 
@@ -78,48 +57,15 @@ for id in tqdm(range(len(candidate_subset_emb))):
 all_candidate_embeddings = np.array(candidate_subset_emb["embeddings"])
 all_candidate_embeddings = torch.from_numpy(all_candidate_embeddings)
 
-
-def compute_scores(emb_one, emb_two):
-    """Computes cosine similarity between two vectors."""
-    scores = torch.nn.functional.cosine_similarity(emb_one, emb_two)
-    return scores.numpy().tolist()
-
-
-def fetch_similar(image, top_k=5):
-    """Fetches the `top_k` similar images with `image` as the query."""
-    # Prepare the input query image for embedding computation.
-    image_transformed = transformation_chain(image).unsqueeze(0)
-    new_batch = {"pixel_values": image_transformed.to(device)}
-
-    # Comute the embedding.
-    with torch.no_grad():
-        query_embeddings = model(**new_batch).last_hidden_state[:, 0].cpu()
-
-    # Compute similarity scores with all the candidate images at one go.
-    # We also create a mapping between the candidate image identifiers
-    # and their similarity scores with the query image.
-    sim_scores = compute_scores(all_candidate_embeddings, query_embeddings)
-    similarity_mapping = dict(zip(candidate_ids, sim_scores))
- 
-    # Sort the mapping dictionary and return `top_k` candidates.
-    similarity_mapping_sorted = dict(
-        sorted(similarity_mapping.items(), key=lambda x: x[1], reverse=True)
-    )
-    id_entries = list(similarity_mapping_sorted.keys())[:top_k]
-
-    ids = list(map(lambda x: int(x.split("_")[0]), id_entries))
-    labels = list(map(lambda x: int(x.split("_")[-1]), id_entries))
-    return ids, labels
-
+# Test image
 test_idx = np.random.choice(len(dataset["test"]))
 test_sample = dataset["test"][test_idx]["image"]
 test_label = dataset["test"][test_idx]["labels"]
 
-sim_ids, sim_labels = fetch_similar(test_sample)
-print(f"Query label: {test_label}")
-print(f"Top 5 candidate labels: {sim_labels}")
+# Fetch similar images
+sim_ids, sim_labels = fetch_similar(test_sample, transformation_chain, device, model, all_candidate_embeddings, candidate_ids, top_k=5)
 
-
+# Plot the images
 def plot_images(images, labels):
     if not isinstance(labels, list):
         labels = labels.tolist()
--- src/utils/transformer_util.py
+++ src/utils/transformer_util.py
@@ -1,6 +1,51 @@
 import torch
 
+def extract_embeddings (model: torch.nn.Module, transformation_chain):
+    """Utility to compute embeddings."""
+    device = model.device
+
+    def pp (batch):
+        images = batch ["image"]
+        # `transformation_chain` is a compostion of preprocessing
+        # transformations we apply to the input images to prepare them
+        # for the model. For more details, check out the accompanying Colab Notebook.
+        image_batch_transformed = torch.stack (
+            [transformation_chain (image) for image in images]
+        )
+        new_batch = {"pixel_values": image_batch_transformed.to (device)}
+        with torch.no_grad ():
+            embeddings = model (**new_batch).last_hidden_state [:, 0].cpu ()
+        return {"embeddings": embeddings}
+
+    return pp
+
 def compute_scores(emb_one, emb_two):
     """Computes cosine similarity between two vectors."""
     scores = torch.nn.functional.cosine_similarity(emb_one, emb_two)
     return scores.numpy().tolist()
+
+def fetch_similar(image, transformation_chain, device, model, candidate_embeddings, candidate_ids, top_k=5):
+    """Fetches the `top_k` similar images with `image` as the query."""
+    # Prepare the input query image for embedding computation.
+    image_transformed = transformation_chain(image).unsqueeze(0)
+    new_batch = {"pixel_values": image_transformed.to(device)}
+
+    # Comute the embedding.
+    with torch.no_grad():
+        query_embeddings = model(**new_batch).last_hidden_state[:, 0].cpu()
+
+    # Compute similarity scores with all the candidate images at one go.
+    # We also create a mapping between the candidate image identifiers
+    # and their similarity scores with the query image.
+    sim_scores = compute_scores(candidate_embeddings, query_embeddings)
+    similarity_mapping = dict(zip(candidate_ids, sim_scores))
+ 
+    # Sort the mapping dictionary and return `top_k` candidates.
+    similarity_mapping_sorted = dict(
+        sorted(similarity_mapping.items(), key=lambda x: x[1], reverse=True)
+    )
+    id_entries = list(similarity_mapping_sorted.keys())[:top_k]
+
+    ids = list(map(lambda x: int(x.split("_")[0]), id_entries))
+    labels = list(map(lambda x: int(x.split("_")[-1]), id_entries))
+    return ids, labels
